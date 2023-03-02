d4aed8be18697a9d591d46ad6c65c74a8b9debf5
professorsugoi <emosock@live.com>
2023-03-02 15:43:55 -0500

    fix deps

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,12 +5,10 @@ import sanity from 'astro-sanity';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		sanity({
-			projectId: 'cj8je4w1',
-			dataset: 'production',
-			apiVersion: 'v2021-03-25',
-			useCdn: true,
-		}),
-	],
+  integrations: [sanity({
+    projectId: 'cj8je4w1',
+    dataset: 'production',
+    apiVersion: 'v2021-03-25',
+    useCdn: true
+  })]
 });
